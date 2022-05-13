5a761a4798a962dcf18a457a478830cfda364395
TomerBrown <73110109+TomerBrown@users.noreply.github.com>
2022-05-13 23:54:31 +0300

    Added Some Tests for functionallity

--- match_app/tests.py
+++ match_app/tests.py
@@ -1,3 +1,85 @@
-from django.test import TestCase
+from django.test import TestCase , Client
+import json
+from .views import error_messages
+from django.http import JsonResponse
+from .models import Job, Candidate, Skill
 
-# Create your tests here.
+
+def get_data_dic(title, top=None):
+    data = {
+        "title": title
+    }
+    if top is not None:
+        data['top'] = top
+    return data
+
+
+class MatchUpTests(TestCase):
+    # Load some pre-defined database (with few entries) to test our app's basic functionality
+    fixtures = ['match_app/fixtures/match_app.yaml']
+    client = Client()
+
+    def get_request(self, data: dict) -> JsonResponse:
+        return self.client.generic("GET", '/match/', data=json.dumps(data), content_type='application/json')
+
+    def check_errors(self, res: JsonResponse, err_msg: str, job_name: str = ""):
+        self.assertEqual(res.status_code, 400)
+        if err_msg != 'invalid_job':
+            self.assertEqual(res.json()["Error"], error_messages[err_msg])
+        else:
+            self.assertEqual(res.json()["Error"], error_messages['invalid_job'].format(title=job_name))
+
+    def test_job_does_not_exists(self):
+        job_name = "Head of Fun"
+        res = self.get_request(get_data_dic(job_name))
+        self.check_errors(res, 'invalid_job', job_name)
+
+
+    def test_incorrect_method(self):
+        res = self.client.generic("PUT", '/match/', data=json.dumps(get_data_dic("Software Engineer")), content_type='application/json')
+        self.check_errors(res, 'invalid_req')
+
+
+    def test_invalid_parameter(self):
+        data = {"Title": "Software Engineer"}
+        res = self.get_request(data)
+        self.check_errors(res, 'invalid_req')
+
+    def test_legit_request_works_and_sorted(self):
+        title = "Software Engineer"
+        res = self.get_request(get_data_dic(title))
+        self.assertEqual(res.status_code, 200)
+        top_candidates = res.json()["top candidates"]
+
+        # Make sure every candidate is indeed with the right title, and returned list is sorted.
+        for i, candidate in enumerate(top_candidates):
+            self.assertEqual(candidate['title'], title)
+            if i < len(top_candidates)-1:
+                self.assertTrue(candidate['num_of_skills_required'] >= top_candidates[i+1]['num_of_skills_required'])
+
+    def test_legit_request_with_change_top(self):
+        """ Test whether the mechanism of returning the top k candidates works"""
+        title = "Software Engineer"
+        num_engineers = len(Candidate.objects.filter(title__title=title))
+        for top in range(1, 10):
+            res = self.get_request(get_data_dic(title , top))
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(len(res.json()["top candidates"]), min(top, num_engineers))
+
+    def test_non_legit_request_with_change_top(self):
+        title = "Software Engineer"
+        for top in range(-5, 1):
+            res = self.get_request(get_data_dic(title,top))
+            self.check_errors(res, 'invalid_req')
+
+    def test_job_with_no_candidates(self):
+        res = self.get_request(get_data_dic("God"))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.json()['top candidates']), 0)
+
+    def test_another_job(self):
+        title = "Finance"
+        res = self.get_request(get_data_dic(title))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.json()['top candidates']), len(Candidate.objects.filter(title__title=title)))
+
--- match_app/views.py
+++ match_app/views.py
@@ -6,7 +6,7 @@ import json
 
 error_messages = {
     "invalid_req": 'The request is invalid. please check the format. '
-                   'should be GET request with json parameters as follows:{title:<title_name>}',
+                   'should be GET request with json parameters as follows:{title:<title_name> , top: <int> (optional)}',
     "invalid_job": 'There is no such job as {title} in the database. Please try a different job title'
 }
 
@@ -14,21 +14,22 @@ error_messages = {
 def is_valid_request(request: HttpRequest):
     """ Given a django HTTP Request returns whether it is valid request """
 
-    # Check if it the correct method
     if request.method != 'GET':
         return False
 
-    # Check if it contains the title (Other parameters will be ignored)
     req = json.loads(request.body.decode('utf-8'))
     if 'title' not in req:
         return False
+
+    if 'top' in req:
+        if not type(req['top']) is int or req['top'] <= 0:
+            return False
 
     return True
 
 
 def find_matches(request: HttpRequest):
     """ The main view that deals with the request and queries the database """
-    TOP = 10
     # First check whether the request is valid (type and content of request)
     if not is_valid_request(request):
         return JsonResponse({"Error": error_messages['invalid_req']}, status=400)
@@ -36,6 +37,7 @@ def find_matches(request: HttpRequest):
     # Take the request parameter - title
     req = json.loads(request.body.decode('utf-8'))
     job_title = req["title"]
+    top = req["top"] if "top" in req else 10
 
     # Try to find the title in the database and return an error message if it does not exists
     try:
@@ -43,24 +45,13 @@ def find_matches(request: HttpRequest):
     except Job.DoesNotExist:
         return JsonResponse({"Error": error_messages['invalid_job'].format(title=job_title)}, status=400)
 
-    # Get all skills of the given job
     skills_required_for_job = job.skills.all()
 
-    # candidate_to_num_of_skills = []
-    # all_candidates = Candidate.objects.all()
-    # for candidate in all_candidates:
-    #     candidate_to_num_of_skills.append({
-    #         "first_name": candidate.first_name,
-    #         "last_name": candidate.last_name,
-    #         "num_of_required_skills": candidate.count_required_skills(skills_required_for_job)
-    #     })
-    # candidate_to_num_of_skills.sort(key=lambda x: x["num_of_required_skills"], reverse=True)
-
     # query the dataset to get candidates with maximal number of required skills
-    sorted_candidates = Candidate.objects.filter(title=Job.objects.get(title=job_title))
+    # Firstly, take those who fit the job title
+    sorted_candidates = Candidate.objects.filter(title__title=job_title)
+    # Secondly , Over those who got the correct title ,sort candidates by number of required skills for job
     sorted_candidates = sorted_candidates.annotate(num_skills=Count('skills', filter=Q(skills__in=skills_required_for_job))).order_by('-num_skills')
 
-
-    # Return the list of top candidates (w.r.t the number of required skills they have)
-    top_candidates = [candidate.to_json(candidate.num_skills) for candidate in sorted_candidates[:TOP]]
+    top_candidates = [candidate.to_json(candidate.num_skills) for candidate in sorted_candidates[:top]]
     return JsonResponse({"top candidates": top_candidates}, content_type='application/json')
