56cbc0c5f2d5c1e68f21e05cd51e04c4dfdc4aa8
TomerBrown <73110109+TomerBrown@users.noreply.github.com>
2022-05-14 16:37:50 +0300

    Remaking Migrations

--- match_app/models.py
+++ match_app/models.py
@@ -7,7 +7,6 @@ MAX_LEN = 128
 class Skill (models.Model):
 
     name = models.CharField(max_length=MAX_LEN)
-    id = models.BigAutoField(primary_key=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +20,6 @@ class Job (models.Model):
 
     title = models.CharField(max_length=128)
     skills = models.ManyToManyField(Skill)
-    id = models.BigAutoField(primary_key=True)
 
     def __str__(self):
         return self.title
@@ -33,7 +31,6 @@ class Candidate (models.Model):
     last_name = models.CharField(max_length=MAX_LEN)
     title = models.ForeignKey(Job, on_delete=models.CASCADE)
     skills = models.ManyToManyField(Skill)
-    id = models.BigAutoField(primary_key=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
